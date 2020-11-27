c41baabb3a09f71fbf050c2fe99898f9c11d2a86
=RHL <rahul.singh@infinity-ventures.com>
2020-11-27 13:15:10 +0530

    initial commit

---
+++ src/main/java/com/jenkins/sample/JenkinsDockerSampleApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jenkins/sample/JenkinsDockerSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkins.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
